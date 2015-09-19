16d3b0f2e0113876f6417f832f76fec3fe01965b
Vince Au <vinceau09@gmail.com>
2015-09-19 23:53:40 +1000

    Add button to enable/disable fullscreen

--- static/scripts.js
+++ static/scripts.js
@@ -1,3 +1,4 @@
+//method for :offscreen method
 jQuery.expr.filters.offscreen = function(el) {
     return (
             (el.offsetLeft + el.offsetWidth) < 0 
@@ -5,3 +6,31 @@ jQuery.expr.filters.offscreen = function(el) {
             || (el.offsetLeft > window.innerWidth || el.offsetTop > window.innerHeight)
            );
 };
+
+// Find the right method, call on correct element
+function launchIntoFullscreen(element) {
+  if(element.requestFullscreen) {
+    element.requestFullscreen();
+  } else if(element.mozRequestFullScreen) {
+    element.mozRequestFullScreen();
+  } else if(element.webkitRequestFullscreen) {
+    element.webkitRequestFullscreen();
+  } else if(element.msRequestFullscreen) {
+    element.msRequestFullscreen();
+  }
+}
+
+// Whack fullscreen
+function exitFullscreen() {
+  if(document.exitFullscreen) {
+    document.exitFullscreen();
+  } else if(document.mozCancelFullScreen) {
+    document.mozCancelFullScreen();
+  } else if(document.webkitExitFullscreen) {
+    document.webkitExitFullscreen();
+  }
+}
+
+function currentlyFullscreen() {
+    return document.fullscreenElement || document.mozFullScreenElement || document.webkitFullscreenElement;
+}
